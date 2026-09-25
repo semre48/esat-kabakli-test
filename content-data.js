cb387ffd9a7d41d77eb234bcac95811afcd23f42
semre48 <suleyman.emre@melden.com>
2026-09-25 14:34:45 +0300

    Add Deezer and Amazon Music

--- content-data.js
+++ content-data.js
@@ -9,7 +9,9 @@ window.ESAT_SITE_CONTENT = {
   cover: "assets/albums/Album-1.webp",
   appleMusic: "https://music.apple.com/tr/album/%C3%A7ayda%C3%A7%C4%B1ra-yak%C4%B1ls%C4%B1n-single/6813552936",
   spotify: "https://open.spotify.com/intl-tr/track/3pURcNRdGqOIiFBuQiUkw1",
-  youtube: "https://www.youtube.com/watch?v=weyGAmNEKtg&list=OLAK5uy_maOdd2NAvkbqw-tSA5vTnu_kIvKAxbF-g"
+  youtube: "https://www.youtube.com/watch?v=weyGAmNEKtg&list=OLAK5uy_maOdd2NAvkbqw-tSA5vTnu_kIvKAxbF-g",
+  deezer: "https://www.deezer.com/tr/album/1081873532",
+  amazonMusic: "https://music.amazon.com/albums/B0HK91RHFS"
 },
     {
       title: "Fetih Müjdesi-  Single",
@@ -17,7 +19,9 @@ window.ESAT_SITE_CONTENT = {
       cover: "assets/albums/Album-2.webp",
       appleMusic: "https://music.apple.com/tr/album/fetih-m%C3%BCjdesi/6766591620?i=6766592019",
       spotify: "https://open.spotify.com/intl-tr/track/2db7YoiUpMnYMJsxRCHH1B",
-      youtube: "https://www.youtube.com/watch?v=OO3fyO71j9s&list=RDOO3fyO71j9s&start_radio=1"
+      youtube: "https://www.youtube.com/watch?v=OO3fyO71j9s&list=RDOO3fyO71j9s&start_radio=1",
+      deezer: "https://www.deezer.com/tr/album/976296141",
+      amazonMusic: "https://music.amazon.com/albums/B0GZLF84HR"
     },
     {
       title: "Seyit Onbaşılar Yahya Çavuşlar - Single",
@@ -25,7 +29,9 @@ window.ESAT_SITE_CONTENT = {
       cover: "assets/albums/Album-3.webp",
       appleMusic: "https://music.apple.com/tr/album/seyit-onba%C5%9F%C4%B1lar-yahya-%C3%A7avu%C5%9Flar-single/1882335321",
       spotify: "https://open.spotify.com/search/Seyit%20Onba%C5%9F%C4%B1lar%20Yahya%20%C3%87avu%C5%9Flar",
-      youtube: "https://www.youtube.com/watch?v=BMF0RYLCvWE&list=RDBMF0RYLCvWE&start_radio=1"
+      youtube: "https://www.youtube.com/watch?v=BMF0RYLCvWE&list=RDBMF0RYLCvWE&start_radio=1",
+      deezer: "https://www.deezer.com/tr/album/932399951",
+      amazonMusic: "https://music.amazon.com/albums/B0GR63XSBF"
     },
      {
       title: "Altaylardan Tuna'ya (Enstrümantal)",
@@ -34,6 +40,8 @@ window.ESAT_SITE_CONTENT = {
       appleMusic: "https://music.apple.com/tr/album/altaylardan-tunaya-enstr%C3%BCmantal-ep/1648119297",
       spotify: "https://open.spotify.com/intl-tr/album/6dZb8XdVbPHWwyyRzgnirO",
       youtube: "https://www.youtube.com/watch?v=24tzVNtJ23Q&list=RD24tzVNtJ23Q&start_radio=1",
+      deezer: "https://www.deezer.com/tr/album/362697427",
+      amazonMusic: "https://music.amazon.com/albums/B0BH533N4M"
      },
       {
       title: "Altaylardan Tuna'ya - EP",
@@ -41,7 +49,9 @@ window.ESAT_SITE_CONTENT = {
       cover: "assets/albums/Album-4.webp",
       appleMusic: "https://music.apple.com/tr/album/altaylardan-tunaya-ep/1587339996",
       spotify: "https://open.spotify.com/intl-tr/album/5L5F9oOHAzYmrDosYBfBHP",
-      youtube: "https://www.youtube.com/watch?v=24tzVNtJ23Q&list=RD24tzVNtJ23Q&start_radio=1"
+      youtube: "https://www.youtube.com/watch?v=24tzVNtJ23Q&list=RD24tzVNtJ23Q&start_radio=1",
+      deezer: "https://www.deezer.com/tr/album/261167542",
+      amazonMusic: "https://music.amazon.com/albums/B09H393RG9"
     },
     {
       title: "Bil Oğlum / Bu Bayrak İnmez (Enstrümantal)",
@@ -49,7 +59,9 @@ window.ESAT_SITE_CONTENT = {
       cover: "assets/albums/Album-6.webp",
       appleMusic: "https://music.apple.com/tr/album/bil-o%C4%9Flum-bu-bayrak-i-nmez-enstr%C3%BCmantal/1561643004",
       spotify: "https://open.spotify.com/intl-tr/track/26aALGlfORNGjsaABkoDcO",
-      youtube: "https://www.youtube.com/watch?v=jHWoShNENXM&list=PLaU8Fgj7y2jsXnfp4ykq3MuSJyfJ2mYUm"
+      youtube: "https://www.youtube.com/watch?v=jHWoShNENXM&list=PLaU8Fgj7y2jsXnfp4ykq3MuSJyfJ2mYUm",
+      deezer: "https://www.deezer.com/tr/album/220518252",
+      amazonMusic: "https://music.amazon.com/albums/B091RCMKS5"
     },
     {
       title: "Bu Bayrak (Mt Remix)",
@@ -57,7 +69,9 @@ window.ESAT_SITE_CONTENT = {
       cover: "assets/albums/Album-7.webp",
       appleMusic: "https://music.apple.com/tr/album/bu-bayrak-mt-remix/1549154496?i=1549154507",
       spotify: "https://open.spotify.com/intl-tr/track/0P6dCTzt7G6Kp9myNywGk8",
-      youtube: "https://www.youtube.com/watch?v=UCAFUNyLNpw&list=RDUCAFUNyLNpw&start_radio=1"
+      youtube: "https://www.youtube.com/watch?v=UCAFUNyLNpw&list=RDUCAFUNyLNpw&start_radio=1",
+      deezer: "https://www.deezer.com/tr/album/200030502",
+      amazonMusic: "https://music.amazon.com/albums/B08T3T8S4C"
     },
     {
       title: "Bil Oğlum (Mt Remix)",
@@ -65,7 +79,9 @@ window.ESAT_SITE_CONTENT = {
       cover: "assets/albums/Album-8.webp",
       appleMusic: "https://music.apple.com/tr/album/bil-o%C4%9Flum-mt-remix-single/1546618306",
       spotify: "https://open.spotify.com/intl-tr/track/1KoTWwhEhiROmzu6cUQ0I7",
-      youtube: "https://www.youtube.com/watch?v=qug03D3qwZQ&list=RDqug03D3qwZQ&start_radio=1"
+      youtube: "https://www.youtube.com/watch?v=qug03D3qwZQ&list=RDqug03D3qwZQ&start_radio=1",
+      deezer: "https://www.deezer.com/tr/album/196068132",
+      amazonMusic: "https://music.amazon.com/albums/B08RDZHRBK"
     },
     {
       title: "Bil Oğlum/Bu Vatan Bölünmez Bu Bayrak İnmez",
@@ -73,7 +89,9 @@ window.ESAT_SITE_CONTENT = {
       cover: "assets/albums/Album-9.webp",
       appleMusic: "https://music.apple.com/tr/album/bil-o%C4%9Flum-bu-vatan-b%C3%B6l%C3%BCnmez-bu-bayrak-i-nmez/1444360777",
       spotify: "https://open.spotify.com/intl-tr/album/4tukcDJ5jm7xQol0d5SfDc",
-      youtube: "https://www.youtube.com/watch?v=kn4f6RH-IwU&list=OLAK5uy_mp968G0rqCgxAEsXRdgBB4K2Z_7uUJUSc&index=2"
+      youtube: "https://www.youtube.com/watch?v=kn4f6RH-IwU&list=OLAK5uy_mp968G0rqCgxAEsXRdgBB4K2Z_7uUJUSc&index=2",
+      deezer: "https://www.deezer.com/tr/album/220518252",
+      amazonMusic: "https://music.amazon.com/albums/B07L54NTPY"
     },
      {
       title: "Sürgün",
@@ -81,7 +99,9 @@ window.ESAT_SITE_CONTENT = {
       cover: "assets/albums/Album-10.webp",
       appleMusic: "https://music.apple.com/tr/album/s%C3%BCrg%C3%BCn/1414400199",
       spotify: "https://open.spotify.com/intl-tr/album/4BYi8Bk0sFgembN7siR7b2",
-      youtube: "https://www.youtube.com/watch?v=QdP2DZc0weY&list=OLAK5uy_ms-K6h8enqrxJsKYWlgksP5PJoe6N8peM&index=2"
+      youtube: "https://www.youtube.com/watch?v=QdP2DZc0weY&list=OLAK5uy_ms-K6h8enqrxJsKYWlgksP5PJoe6N8peM&index=2",
+      deezer: "https://www.deezer.com/tr/album/68665621",
+      amazonMusic: "https://music.amazon.com/albums/B07FPQ2WCH"
     },
      {
       title: "Göç",
@@ -89,7 +109,9 @@ window.ESAT_SITE_CONTENT = {
       cover: "assets/albums/Album-11.webp",
       appleMusic: "https://music.apple.com/tr/album/g%C3%B6%C3%A7/1414397953",
       spotify: "https://open.spotify.com/intl-tr/album/5EckTF09x5bjzTt32sZL5c",
-      youtube: "https://www.youtube.com/watch?v=Y3KVnNOmdQs&list=OLAK5uy_lGZR0-9TZBOG8Ze2bA4CU5KZvIzQEpZwQ&index=3"
+      youtube: "https://www.youtube.com/watch?v=Y3KVnNOmdQs&list=OLAK5uy_lGZR0-9TZBOG8Ze2bA4CU5KZvIzQEpZwQ&index=3",
+      deezer: "https://www.deezer.com/tr/album/68666311",
+      amazonMusic: "https://music.amazon.com/albums/B07FPRYGHW"
     },
      {
       title: "Kirve Memi / Gün Gidende Ay Gelende",
@@ -97,7 +119,9 @@ window.ESAT_SITE_CONTENT = {
       cover: "assets/albums/Album-12.webp",
       appleMusic: "https://music.apple.com/tr/album/kirve-memi/1414378318",
       spotify: "https://open.spotify.com/intl-tr/album/3teM5Bfasxpx4P95xgsakA",
-      youtube: "https://www.youtube.com/watch?v=f9k5QPikir0&list=OLAK5uy_mUmbrKHWs3HQO6DdRwLnIBrXYAoNDuywU&index=2"
+      youtube: "https://www.youtube.com/watch?v=f9k5QPikir0&list=OLAK5uy_mUmbrKHWs3HQO6DdRwLnIBrXYAoNDuywU&index=2",
+      deezer: "https://www.deezer.com/tr/album/68665841",
+      amazonMusic: "https://music.amazon.com/albums/B07FPTS7W2"
     },
     {
       title: "Oğul",
@@ -105,7 +129,9 @@ window.ESAT_SITE_CONTENT = {
       cover: "assets/albums/Album-13.webp",
       appleMusic: "https://music.apple.com/tr/album/o%C4%9Ful/1414380170",
       spotify: "https://open.spotify.com/intl-tr/album/03lGagnTDWdBYzdFbgrtbN",
-      youtube: "https://www.youtube.com/watch?v=bc5-99rgRxI&list=PLKN4o56XVwUc7ioCcnMY0gH4lVgm6FCDo"
+      youtube: "https://www.youtube.com/watch?v=bc5-99rgRxI&list=PLKN4o56XVwUc7ioCcnMY0gH4lVgm6FCDo",
+      deezer: "https://www.deezer.com/tr/album/68666241",
+      amazonMusic: "https://music.amazon.com/albums/B07FPQKSM8"
     },
   ],
 
